375f9d9c6b7ab079bc1c7deb68e50f96e289da0c
Ben <blearn5241@gmail.com>
2023-03-09 18:28:35 -0500

    added google analytics 4 to basic and enhanced sites

--- .sfdx/typings/lwc/apex/RBNavigationMenuController.d.ts
+++ .sfdx/typings/lwc/apex/RBNavigationMenuController.d.ts
@@ -1,3 +1,3 @@
 declare module "@salesforce/apex/RBNavigationMenuController.getNavigationMenuItems" {
-  export default function getNavigationMenuItems(param: {navigationLinkSetDeveloperName: any, publishStatus: any}): Promise<any>;
+  export default function getNavigationMenuItems(param: {navigationLinkSetDeveloperName: any}): Promise<any>;
 }
